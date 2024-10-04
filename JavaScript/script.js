0bb4c881c6fd7a7e6023ea6cf0832aca5327e633
Priyanhu <priyanshudhiman644@gmail.com>
2024-10-04 11:00:10 +0530

    Design of Netflix

---
+++ JavaScript/script.js
@@ -0,0 +1,23 @@
+
+    //      jQuery is here 
+
+$(document).ready(function(){
+    $("#ques-1").click(function(){
+        $("#ans-1").slideToggle();
+    });
+    $("#ques-2").click(function(){
+        $("#ans-2").slideToggle();
+    });
+    $("#ques-3").click(function(){
+        $("#ans-3").slideToggle();
+    });
+    $("#ques-4").click(function(){
+        $("#ans-4").slideToggle();
+    });
+    $("#ques-5").click(function(){
+        $("#ans-5").slideToggle();
+    });
+    $("#ques-6").click(function(){
+        $("#ans-6").slideToggle();
+    });
+})
